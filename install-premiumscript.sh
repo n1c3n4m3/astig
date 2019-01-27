e094101141aed4117ed070cf0314fae16d497b1b
n1c3n4m3 <30147024+n1c3n4m3@users.noreply.github.com>
2019-01-27 19:41:39 +0800

    Update install-premiumscript.sh

--- install-premiumscript.sh
+++ install-premiumscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Created by http://www.vps-murah.net
-# Modified by shigeno
+# Modified by CriZ
 
 cd
 sed -i '$ i\screen -AmdS limit /root/limit.sh' /etc/rc.local
@@ -21,7 +21,7 @@ cat > /root/limit.sh <<END3
 END3
 
 cd /usr/local/bin
-wget -O premium-script.tar.gz "https://raw.githubusercontent.com/shigeno143/OCSPanelCentos6/master/premium-script.tar.gz"
+wget -O premium-script.tar.gz "https://raw.githubusercontent.com/n1c3n4m3/astig/master/premium-script.tar.gz"
 tar -xvf premium-script.tar.gz
 rm -f premium-script.tar.gz
 
@@ -77,6 +77,6 @@ cd
 echo " "
 echo " "
 echo "Premium Script Successfully Update!"
-echo "Modified by shigeno"
+echo "Modified by CriZ"
 echo " "
 echo " "
